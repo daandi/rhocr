b78cd1b8144f193735a82a5a8bfe3b8cff737428
Andreas Neumann <andreas.neumann@an-it.com>
2011-07-03 20:21:38 +0200

    ' und " in hocr erlaubt

--- lib/ocr_page.rb
+++ lib/ocr_page.rb
@@ -10,10 +10,10 @@ class OCRPage < OCRBox
     
     def hocr_lines( hocr_contents)
         hocr_array = []
-        for line in  hocr_contents.split(/<span class="ocr_line"/) do
+        for line in  hocr_contents.split(/<span class=['"]ocr_line['"]/) do
             line_array = []
-            for ocrx_word in line.scan(/<span class="ocrx_word"[^>]+>[^<]+<\/span>/) do
-                ocrx_word =~ /title="bbox (\d+) (\d+) (\d+) (\d+)">([^<]+)</
+            for ocrx_word in line.scan(/<span class=['"]ocrx_word['"][^>]+>[^<]+<\/span>/) do
+                ocrx_word =~ /title=['"]bbox (\d+) (\d+) (\d+) (\d+)['"]>([^<]+)</
                 current_word = OCRXWord.new($1,$2,$3,$4,$5)
                 line_array << current_word
             end
