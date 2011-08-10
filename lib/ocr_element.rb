572bc6e333f040682a5f087c2c606af177a212b2
Andreas Neumann <andreas.neumann@an-it.com>
2011-08-10 22:13:20 +0200

    ocr_element Fortschritte

--- lib/ocr_element.rb
+++ lib/ocr_element.rb
@@ -1,20 +1,40 @@
 #coding:utf-8
 
 require_relative 'hocr_box'
-require 'nokogiri'
+
 
 class OCRElement < HOCRBox
     
-    attr_reader :ocr_class, :children, :coordinates
+    attr_reader :ocr_class, :children
     
-    def initialize(ocr_class, coordinates, children) 
-        @ocr_class = ocr_class
-        @coordinates = coordinates
-        @children = children
+    
+    def initialize(ocr_element_html)
+        
+        super( extract_coordinates(ocr_element_html) )
+        @ocr_class = extract_ocr_class(ocr_element_html)
+        @children = extract_children(ocr_element_html)
     end
     
     def to_s
         "#{@ocr_class}:#{super}"
+    end
+    
+    def extract_coordinates(ocr_element_html)
+        pos = ocr_element_html['title']
+        pos =~ /bbox (\d+) (\d+) (\d+) (\d+)/
+        [$1, $2, $3, $4]
+    end
+    
+    def extract_ocr_class(ocr_element_html)
+        ocr_element_html['class']
+    end
+    
+    def extract_children(ocr_element_html)
+        children = []
+        for child_fragment_html in ocr_element_html.children do
+            children << OCRElement.new(child_fragment_html)
+        end
+        children
     end
     
 end
--- rspec/ocr_element_spec.rb
+++ rspec/ocr_element_spec.rb
@@ -2,12 +2,14 @@
 
 require_relative '../lib/ocr_element'
 require 'nokogiri'
+
+require 'pp'
 describe OCRElement do
   
   before(:each) do
       html_string = "<p class='ocr_par' title='bbox 79 109 1119 189' style='font-size:8pt;font-family:Arial;font-style:normal'><span class='ocr_line' title='bbox 79 109 1119 145'><span class='ocrx_word' title='bbox 79 109 294 144'>Athenobius,</span> <span class='ocrx_word' title='bbox 334 112 398 139'>Der</span> <span class='ocrx_word' title='bbox 417 115 476 139'>von</span> <span class='ocrx_word' title='bbox 494 112 545 139'>der</span> <span class='ocrx_word' title='bbox 565 112 687 140'>Göttin</span> <span class='ocrx_word' title='bbox 707 112 857 140'>Minerva</span> <span class='ocrx_word' title='bbox 876 112 954 145'>lebt,</span> <span class='ocrx_word' title='bbox 974 112 1043 140'>oder:</span> <span class='ocrx_word' title='bbox 1062 112 1119 140'>Mi»</span><br></span><span class='ocr_line' title='bbox 108 155 300 189'><span class='ocrx_word' title='bbox 108 159 183 182'>nerva</span> <span class='ocrx_word' title='bbox 201 155 300 189'>Bogen.</span></span></p>"
       
-      @html_fragment = Nokogiri::HTML(html_string)
+      @html_fragment = Nokogiri::HTML::fragment(html_string).child
       @ocr_element = OCRElement.new(@html_fragment)
   end
   
@@ -15,8 +17,11 @@ describe OCRElement do
       it 'should create an element from html input' do
           @ocr_element
       end
-      it 'should have a class' do
+      it 'should have an ocr_class' do
            @ocr_element.ocr_class.should == 'ocr_par'
+      end
+      it 'should have coordinates' do
+           @ocr_element.coordinates.should == [79, 109, 1119, 189]
       end
       it 'should have children' do
           for child in @ocr_element.children do
